be41994bdeb183f0c24ab407818f2b670928cb1a
VictorSantos09 <110687512+VictorSantos09@users.noreply.github.com>
2022-09-17 21:44:15 -0300

    Add files via upload

---
+++ TrabalhoMercado/TrabalhoMercado/Mercado.cs
@@ -0,0 +1,84 @@
+﻿namespace TrabalhoMercado
+{
+    public class Mercado
+    {
+        public int Buying(string[] produtos, int MinimalQtd, int boughtProducts, string[] workers, string[] paymentChoice, int billing, int random, int outputQtd)
+        {
+            Console.Clear();
+            Console.WriteLine("ESCOLHA SUA OPÇÂO:\n");
+            int contador = 1;
+            for (int i = 0; i < produtos.Length; i++)
+            {
+                Console.WriteLine($"{contador} - {produtos[i]}");
+                contador++;
+            }
+            string userChoiceProduct = Console.ReadLine();
+            while (userChoiceProduct.Length > 5)
+            {
+                Console.WriteLine("Tente novamente");
+                userChoiceProduct = Console.ReadLine();
+            }
+            if (userChoiceProduct == "1")
+            {
+                outputQtd = BuyPattern(0, MinimalQtd, boughtProducts, produtos, workers, paymentChoice, billing, random);
+            }
+            if (userChoiceProduct == "2")
+            {
+                outputQtd = BuyPattern(1, MinimalQtd, boughtProducts, produtos, workers, paymentChoice, billing, random);
+            }
+            if (userChoiceProduct == "3")
+            {
+                outputQtd = BuyPattern(2, MinimalQtd, boughtProducts, produtos, workers, paymentChoice, billing, random);
+            }
+            if (userChoiceProduct == "4")
+            {
+                outputQtd = BuyPattern(3, MinimalQtd, boughtProducts, produtos, workers, paymentChoice, billing, random);
+            }
+            if (userChoiceProduct == "5")
+            {
+                outputQtd = BuyPattern(4, MinimalQtd, boughtProducts, produtos, workers, paymentChoice, billing, random);
+            }
+            return outputQtd;
+        }
+        public int BuyPattern(int item, int MinimalQtd, int boughtProducts, string[] produtos, string[] workers, string[] paymentChoice, int billing, int random)
+        {
+            int userInputQtd = 0;
+            Console.WriteLine($"Escolhido: {produtos[item]} | Valor unidades: R${random} \n");
+            Console.WriteLine($"Quantia Minima {MinimalQtd}\nDigite a quantidade:");
+            string userInputQtdST = Console.ReadLine();
+            userInputQtd = Convert.ToInt32(userInputQtdST);
+            if (userInputQtd < MinimalQtd)
+            {
+                Console.WriteLine("Quantia invalida");
+            }
+            else
+            {
+                Console.WriteLine("Digite 1 para finalizar sua compra ou 2 para trocar a compra:"); //SOMENTE IMPRIMI NA TELA (CONT. NA PROGRAM)
+            }
+            return userInputQtd;
+        }
+        public int Payment(string[] paymentChoice)
+        {
+            int contador = 1;
+            Console.WriteLine($"Escolha sua forma de pagamento: ");
+            for (int i = 0; i < paymentChoice.Length; i++)
+            {
+                Console.WriteLine($"{contador} - {paymentChoice[i]}");
+                contador++;
+            }
+            string userInputPaymentST = Console.ReadLine();
+            int userInputPayment = Convert.ToInt32(userInputPaymentST);
+            return userInputPayment;
+        }
+        public void Fechamento(int caixa, int billing, int boughtProducts, int userInputQtd, int random)
+        {
+            // NECESSARIO userInputQtd PARA PODER SOMAR O PRODUTOS COMPRADOS
+            caixa = caixa + billing;
+            Console.WriteLine($"Quantia atual no caixa: {caixa}");
+            Console.WriteLine($"Quantidade produtos comprados: {boughtProducts} | Valor total R${billing}");
+            Console.WriteLine("Clique ENTER para fechar o caixa");
+            Console.ReadKey();
+            Console.WriteLine("Caixa fechado com sucesso!");
+        }
+    }
+}
---
+++ TrabalhoMercado/TrabalhoMercado/Program.cs
@@ -0,0 +1,58 @@
+﻿using TrabalhoMercado;
+Mercado mercado = new Mercado();
+Random random = new Random();
+
+string[] paymentChoice = new string[3];
+string[] workers = new string[2];
+string[] produtos = new string[5];
+int[] despesa = new int[3];
+int boughtProducts = 0;
+int userInputQtdReturned = 0;
+int outputQtd = 0;
+int billing = 0;
+int caixa = 50;
+int MinimalQtd = 100;
+
+produtos[0] = "Maça";
+produtos[1] = "Picanha";
+produtos[2] = "Detergente";
+produtos[3] = "Ração";
+produtos[4] = "Fini";
+workers[0] = "João";
+workers[1] = "Carina";
+paymentChoice[0] = "Crédito";
+paymentChoice[1] = "Débito";
+paymentChoice[2] = "Dinheiro";
+
+Console.WriteLine("Supermercado HM\n");
+bool userUsing = true;
+while (userUsing)
+{
+    int randomReturned = random.Next(20, 40);
+    Console.WriteLine("1 - Comprar\n2- Checar o caixa\n3 - Encerrar\n");
+    var userInputChoice = Console.ReadLine();
+    if (userInputChoice == "1")
+    {
+        userInputQtdReturned = mercado.Buying(produtos, MinimalQtd, boughtProducts, workers, paymentChoice, billing, randomReturned, outputQtd);
+        string choice = Console.ReadLine();
+        if (choice == "1")
+        {
+            int billingUser = 0;
+            int PaymentReturned = mercado.Payment(paymentChoice);
+            boughtProducts = boughtProducts + userInputQtdReturned;
+            billing = billing + userInputQtdReturned * randomReturned;
+            billingUser = billingUser + userInputQtdReturned * randomReturned;
+            Console.WriteLine($"{workers[0]} efetuou sua compra no valor de R${billingUser} no {paymentChoice[--PaymentReturned]} com sucesso, {workers[1]} empacotou sua compra. Aproveite!");
+        }
+    }
+    if (userInputChoice == "2")
+    {
+        mercado.Fechamento(caixa, billing, boughtProducts, userInputQtdReturned, randomReturned);
+    }
+    if (userInputChoice == "3")
+    {
+        Console.Clear();
+        Console.WriteLine("Até a proxima!");
+        userUsing = false;
+    }
+}
